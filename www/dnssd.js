fb152cab723f8fc982f1bba2154200095934acce
michuNEEO <michael@neeo.com>
2016-02-01 14:00:29 +0100

    increase timeout, some styling changes

--- www/dnssd.js
+++ www/dnssd.js
@@ -10,7 +10,7 @@
 var resolveQueue = [];
 var isResolving = false;
 
-var RESOLVE_TIMEOUT = 5000;//ms
+var RESOLVE_TIMEOUT_MS = 8000;
 
 function processResolveQueue() {
     if (isResolving || resolveQueue.length === 0){
@@ -31,18 +31,17 @@ function processResolveQueue() {
         console.error("Plugin cordova-dnssd, service resolve failed : ",result);
         if (queueItem.errorCallback)
             queueItem.errorCallback(result,queueItem.serviceName,queueItem.regType,queueItem.domain);
-    }
+    };
 
     var resolveTimeOut = setTimeout(function(){
-        console.error("Service resolve timed out:",queueItem.serviceName);
+        console.error("Service resolve timed out:", queueItem.serviceName);
         timedOut = true;
         resolveFailed('TIMEOUT');
 
         // Don't resume processing. We can only resolve 1 service at a time so we need to wait until native function return or we'll get an error.
-    },RESOLVE_TIMEOUT);
+    }, RESOLVE_TIMEOUT_MS);
 
-    function success(result)
-    {
+    function success(result) {
         if (!timedOut) {
             // Don't do anything if resolve timed out.
             clearTimeout(resolveTimeOut);
@@ -62,17 +61,15 @@ function processResolveQueue() {
     cordova.exec(success, resolveFailed, "fi.peekpoke.cordova.dnssd", "resolve", [queueItem.serviceName, queueItem.regType, queueItem.domain]);
 }
 
-function DNSSD()
-{
-}
+function DNSSD() {}
 
 DNSSD.prototype.stopBrowsing = function(callback) {
     resolveQueue.length = 0;
     var stopBrowsingFailed = function(result){
         console.error("Plugin cordova-dnssd, service stopBrowsing failed : ",result);
-    }
+    };
     cordova.exec(callback, stopBrowsingFailed, "fi.peekpoke.cordova.dnssd", "stopBrowsing", []);
-}
+};
 
 DNSSD.prototype.browse=function(regType, domain, serviceFound, serviceLost) {
     function success(result)
@@ -85,9 +82,9 @@ DNSSD.prototype.browse=function(regType, domain, serviceFound, serviceLost) {
 
     var browsingFailed = function(result){
         console.error("Plugin cordova-dnssd, service browse failed : ",result);
-    }
+    };
     cordova.exec(success, browsingFailed, "fi.peekpoke.cordova.dnssd", "browse", [regType, domain]);
-}
+};
 
 DNSSD.prototype.resolve=function(serviceName, regType, domain, serviceResolved, errorCallback) {
     resolveQueue.push({
@@ -98,8 +95,8 @@ DNSSD.prototype.resolve=function(serviceName, regType, domain, serviceResolved, 
         domain: domain
     });
 
-    processResolveQueue()
-}
+    processResolveQueue();
+};
 
 module.exports = new DNSSD();
 
